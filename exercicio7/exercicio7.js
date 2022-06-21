76ca8ad373824f65900b14d47f619badaf369cc5
unknown <glopes.p09@gmail.com>
2022-06-21 15:19:14 -0300

    Exercicio 7

---
+++ exercicio7/exercicio7.js
@@ -0,0 +1,13 @@
+let familia = [{nome:"Fernanda", idade: 16, corFavorita:"Azul"},
+ {nome:"Lucas", idade:9, corFavorita:"verde"},
+{nome:"Jade", idade:30, corFavorita:"Vinho"}];
+let i = 0
+
+function objetos (familia) {
+    while(i < familia.length) {
+        console.log(familia[i]);
+        i++
+    }
+
+}
+objetos(familia)
